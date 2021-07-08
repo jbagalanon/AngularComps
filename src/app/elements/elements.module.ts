e9f93ed4d16123312ce940ac2b1153634fc1cb91
Joemar Bagalanon <jbagalanon@gmail.com>
2021-07-09 02:16:16 +0800

    modify widget components and export to elements component

--- src/app/elements/elements.module.ts
+++ src/app/elements/elements.module.ts
@@ -1,3 +1,4 @@
+import { SharedModule } from './../shared/shared.module';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
@@ -6,9 +7,10 @@ import { ElementsHomeComponent } from './elements-home/elements-home.component';
 import { PlaceholderComponent } from './placeholder/placeholder.component';
 import { TimesDirective } from './times.directive';
 
+
 @NgModule({
   declarations: [ElementsHomeComponent, PlaceholderComponent, TimesDirective],
-  imports: [CommonModule, ElementsRoutingModule],
+  imports: [CommonModule, ElementsRoutingModule, SharedModule],
   exports: [],
 })
 export class ElementsModule {}
