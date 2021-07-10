9188424ee7b95b6f7ff9781e6b3da5b7532dcd9e
Joemar Bagalanon <jbagalanon@gmail.com>
2021-07-11 00:44:50 +0800

    added data and header in collection home module

--- src/app/collections/collections-home/collections-home.component.ts
+++ src/app/collections/collections-home/collections-home.component.ts
@@ -3,13 +3,23 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-collections-home',
   templateUrl: './collections-home.component.html',
-  styleUrls: ['./collections-home.component.css']
+  styleUrls: ['./collections-home.component.css'],
 })
 export class CollectionsHomeComponent implements OnInit {
+  data = [
+    { name: 'James', age: 23, job: 'Designer' },
+    { name: 'Joemar', age: 31, job: 'Programmer' },
+    { name: 'Jane', age: 27, job: 'Writer' },
+    { name: 'Reymar', age: 19, job: 'Programmer' },
+  ];
 
-  constructor() { }
+  headers = [
+    { key: 'name', label: 'Name' },
+    { key: 'age', label: 'Age' },
+    { key: 'job', label: 'Job' },
+  ];
 
-  ngOnInit(): void {
-  }
+  constructor() {}
 
+  ngOnInit(): void {}
 }
