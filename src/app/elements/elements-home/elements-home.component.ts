b8dd25d82fe8298dccdf86dc35060c8e24570d22
Joemar Bagalanon <jbagalanon@gmail.com>
2021-07-11 00:09:32 +0800

    Damage configuration

--- src/app/elements/elements-home/elements-home.component.ts
+++ src/app/elements/elements-home/elements-home.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-elements-home',
-  templateUrl: './elements-home.component.html',
+  templateUrl:'./elements-home.component.html',
   styleUrls: ['./elements-home.component.css']
 })
 export class ElementsHomeComponent implements OnInit {
